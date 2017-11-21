b99e76a35b464dc5b121e5c3afef60786658e0dd
36115152016.3 <36115152016.3@36DE194561.senacrjedu.local>
2017-11-21 12:00:18 -0200

    aula2

---
+++ src/Exemplo.java
@@ -0,0 +1,11 @@
+
+public class Exemplo {
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y;
+		
+		y = ++x;
+		System.out.println("Saida: \nx: " + x + " e y: " + y + ".");
+	}
+}
---
+++ src/TiposDeDados.java
@@ -0,0 +1,19 @@
+
+public class TiposDeDados {
+
+	public static void main(String[] args){
+		System.out.println("Tipos de dados em Java: \n"
+		+ "\nMenor Byte:" + Byte.MIN_VALUE
+		+ "\nMaior Byte:" + Byte.MAX_VALUE
+		+ "\nMenor Short:" + Short.MIN_VALUE
+		+ "\nMaior Short:" + Short.MAX_VALUE
+		+ "\nMenor Integer:" + Integer.MIN_VALUE
+		+ "\nMaior Integer:" + Integer.MAX_VALUE
+		+ "\nMenor Long:" + Long.MIN_VALUE
+		+ "\nMaior Long:" + Long.MAX_VALUE
+		+ "\nMenor Float:" + Float.MIN_VALUE
+		+ "\nMaior Float:" + Float.MAX_VALUE
+		+ "\nMenor Doulbe:" + Double.MIN_VALUE
+		+ "\nMaior Doulbe:" + Double.MAX_VALUE);	
+	}
+}
